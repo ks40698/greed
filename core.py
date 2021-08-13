a56ba347527d37a413e8f13dfda07b67750e6c7a
ks40698 <77614375+ks40698@users.noreply.github.com>
2021-08-13 19:09:44 +0530

    Update core.py

--- core.py
+++ core.py
@@ -31,25 +31,17 @@ def main():
     log.debug("Set logging level to INFO while the config is being loaded")
 
     # Ensure the template config file exists
-    if not os.path.isfile("config/template_config.toml"):
-        log.fatal("config/template_config.toml does not exist!")
+    if not os.path.isfile("config/config.toml"):
+        log.fatal("config/config.toml does not exist!")
         exit(254)
 
-    # If the config file does not exist, clone the template and exit
-    if not os.path.isfile("config/config.toml"):
-        log.debug("config/config.toml does not exist.")
-
-        with open("config/template_config.toml", encoding="utf8") as template_cfg_file, \
-                open("config/config.toml", "w", encoding="utf8") as user_cfg_file:
-            # Copy the template file to the config file
-            user_cfg_file.write(template_cfg_file.read())
-
+    
         log.fatal("A config file has been created in config/config.toml."
                   " Customize it, then restart greed!")
         exit(1)
 
     # Compare the template config with the user-made one
-    with open("config/template_config.toml", encoding="utf8") as template_cfg_file, \
+    with open("config/config.toml", encoding="utf8") as template_cfg_file, \
             open("config/config.toml", encoding="utf8") as user_cfg_file:
         template_cfg = nuconfig.NuConfig(template_cfg_file)
         user_cfg = nuconfig.NuConfig(user_cfg_file)
