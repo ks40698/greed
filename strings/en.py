68559607e539779eb500c1754aa42373c3a766f1
ks40698 <77614375+ks40698@users.noreply.github.com>
2021-08-13 22:03:42 +0530

    Update en.py

--- strings/en.py
+++ strings/en.py
@@ -312,7 +312,7 @@ edit_current_value = "The current value is:\n" \
 # Payment: cash payment info
 payment_cash = "You can pay money in UPI, Paytm, GPay, PhonePay.\n" \
                "Contact @Kirodewal for Further Steps.\n\n"
-               "Pay at checkout, and give this id to the manager:\n" \
+               "Pay at checkout, and give this id to the manager:" \
                "<b>{user_cash_id}</b>"
 
 # Payment: invoice amount
